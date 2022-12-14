da5c0b28f9795bff6facc495e6d8b42b0bb600ba
Mae Schreck <maedevlin47@gmail.comm>
2022-12-13 20:51:20 -0500

    done

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -2,7 +2,12 @@ class CheesesController < ApplicationController
 
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    render json: cheeses, status: :ok
+  end
+
+  def show
+    cheese = Cheese.find_by(id: params[:id])
+    render json: cheese
   end
 
 end
